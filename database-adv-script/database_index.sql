d5f82e86d5e995120fb1454b118618371c9b15d8
Belal Ibrahim <110784708+BelalEbrahim@users.noreply.github.com>
2025-07-06 03:25:23 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,5 +1,18 @@
--- Task 3: Create Indexes
+-- Task 3: Measure performance before and after adding indexes
+-- 1. Before indexing
+EXPLAIN ANALYZE
+SELECT *
+FROM bookings
+WHERE user_id = 123;
+
+-- 2. Create Indexes
 CREATE INDEX idx_bookings_user_id ON bookings(user_id);
 CREATE INDEX idx_bookings_property_id ON bookings(property_id);
 CREATE INDEX idx_reviews_property_id ON reviews(property_id);
 CREATE INDEX idx_payments_booking_id ON payments(booking_id);
+
+-- 3. After indexing
+EXPLAIN ANALYZE
+SELECT *
+FROM bookings
+WHERE user_id = 123;
